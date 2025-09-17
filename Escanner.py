0110518a6aea93065f8a9f0995067f169e2f6d6f
RaulLeon01 <20243rd013@utez.edu.mx>
2025-09-17 15:56:01 -0600

    Rastreador de repositorios
    Rastrea los repositorios de girhub en un directorio

---
+++ Escanner.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+import webbrowser
+#import requests
+import winreg
+import os
+
+# Configuración del directorio de trabajo de git
+git_dir = r"E:\1-UTEZ\4-Cuatrimestre\ProgramacionRedes\Trabajos2"
+
+# Crea el directorio si no existe
+if not os.path.exists(git_dir):
+    os.makedirs(git_dir)
+    print(f"Created directory: {git_dir}")
+
+# Lista todos los repositorios en el directorio de trabajo
+repos = [name for name in os.listdir(git_dir)
+         if os.path.isdir(os.path.join(git_dir, name)) and
+         os.path.exists(os.path.join(git_dir, name, ".git"))
+]
